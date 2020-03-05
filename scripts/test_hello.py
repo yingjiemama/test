79ca9a3e94c4c11c6d1bde30e43319de74ba124b
yingjiemama <1129422119@qq.com>
2020-03-05 12:52:33 +0800

    添加项目

---
+++ scripts/test_hello.py
@@ -0,0 +1,9 @@
+class TestHello:
+    def test_hello1(self):
+        assert 1
+    def test_hello2(self):
+        assert 1
+    def test_hello3(self):
+        assert 0
+    def test_hello4(self):
+        assert 1
